612f10923bfb2e7134c46906e89cbca3b82ba81c
Nemanja Tesic <nemanjatesic96@gmail.com>
2022-04-09 17:52:54 +0200

    Hotel composite integration

--- microservices/hotel-composite-service/src/main/java/microservices/composite/hotel/HotelCompositeServiceApplication.java
+++ microservices/hotel-composite-service/src/main/java/microservices/composite/hotel/HotelCompositeServiceApplication.java
@@ -2,9 +2,18 @@ package microservices.composite.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan
 public class HotelCompositeServiceApplication {
+	
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelCompositeServiceApplication.class, args);
